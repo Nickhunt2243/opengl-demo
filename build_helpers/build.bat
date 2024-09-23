08157107867335e106c063f7205f8a330b5b2afb
nickhunt2243 <nickrhunt@yahoo.com>
2024-09-24 01:03:48 +0200

    Updating the readme section

---
+++ build_helpers/build.bat
@@ -0,0 +1,9 @@
+::Step 1: Configure the project
+cmake -S . -B build
+
+::Step 2: Build the project
+cmake --build build --target OpenGLDemo --config Debug
+
+:: Move to Debug directory and run program
+cd Debug
+OpenGLDemo.exe
